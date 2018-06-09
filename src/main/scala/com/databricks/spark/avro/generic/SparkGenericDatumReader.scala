32d30f890c37c61330cd2ea2b55404c5e46c47f1
smiklos <szotsmiklos@gmail.com>
2018-06-09 12:53:32 +0200

    fix javadoc

--- src/main/scala/com/databricks/spark/avro/generic/SparkGenericDatumReader.scala
+++ src/main/scala/com/databricks/spark/avro/generic/SparkGenericDatumReader.scala
@@ -3,8 +3,8 @@ package com.databricks.spark.avro.generic
 import org.apache.avro.Schema
 import org.apache.avro.generic.{GenericDatumReader, GenericRecord}
 
-/**
-  * A generic datumreader that reads strings as string instead of utf-8
+ /**
+  *  A generic datumreader that reads strings as string instead of utf-8
   */
 class SparkGenericDatumReader extends GenericDatumReader[GenericRecord]{
 
